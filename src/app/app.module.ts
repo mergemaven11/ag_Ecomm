6b316ecc307a8613c9d7fe1d5df1992a0a9c9736
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2020-02-26 14:34:42 -0500

    Add components, Services | User and Products.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './products/cart/cart.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     AdminProductsComponent,
-    AdminUsersComponent
+    AdminUsersComponent,
+    ProductsComponent,
+    CartComponent,
+    UserComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
