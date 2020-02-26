6de2704a89830b7a81c79de6187ec38cb2da83dc
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2020-02-26 14:14:40 -0500

    First commit | Admin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
